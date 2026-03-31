6acda59ecf004031a812506b27e550b73852ba56
vinicius-rabello <viniciusrabello@matematica.ufrj.br>
2026-03-30 21:20:48 -0300

    added srno to main file

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ TRAIN_MAP = {
     ('dscms',    'default'):    'models.DSCMS.train',
     ('prusr',    'default'):    'models.PRUSR.train',
     ('srcnn',    'default'):    'models.SRCNN.train',
+    ('srno',    'default'):    'models.srno.train',
     ('cyclegan', 'multistage'): 'models.CycleGAN.train',  # update when available
     ('dscms',    'multistage'): 'models.DSCMS.train',     # update when available
     ('prusr',    'multistage'): 'models.PRUSR.train',     # update when available
     ('srcnn',    'multistage'): 'models.SRCNN.msrnn_train',
+    ('srno',    'multistage'):    'models.srno.train',       # update when available
 }
 
 def main():
     parser = argparse.ArgumentParser(description='Train models')
-    parser.add_argument('--model', choices=['cyclegan', 'dscms', 'prusr', 'srcnn'],
+    parser.add_argument('--model', choices=['cyclegan', 'dscms', 'prusr', 'srcnn', 'srno'],
                         help='Model to train')
     parser.add_argument('--training-strategy', choices=['default', 'multistage'],
                         default='default', help='Training strategy to use')
